5763ad88414e1b401109e4d841fc7ff4b6a5bc88
MagonxESP <janma.360@gmail.com>
2024-04-16 08:17:51 +0200

    replace version to 0.0.1

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.magonxesp"
-version = "0.0.0"
+version = "0.0.1"
 
 mavenPublishing {
 	coordinates(group as String, "criteria-core", version as String)
--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -3,10 +3,10 @@
 VERSION=$1
 git cliff --bump -o CHANGELOG.md
 git add CHANGELOG.md
-sed -i "s/version = \"v?[0-9.]+\.?[a-z]*\.?[0-9]*\"/version = \"$VERSION\"/g" core/build.gradle.kts
-sed -i "s/version = \"v?[0-9.]+\.?[a-z]*\.?[0-9]*\"/version = \"$VERSION\"/g" spring-boot/build.gradle.kts
+sed -i .bak "s/version = \"v?[0-9.]+\.?[a-z]*\.?[0-9]*\"/version = \"$VERSION\"/g" core/build.gradle.kts
+sed -i .bak "s/version = \"v?[0-9.]+\.?[a-z]*\.?[0-9]*\"/version = \"$VERSION\"/g" spring-boot/build.gradle.kts
 git add core/build.gradle.kts spring-boot/build.gradle.kts
-sed -i -r "s/io\.github\.magonxesp:([a-z\-]+):v?[0-9.]+\.?[a-z]*\.?[0-9]*/io\.github\.magonxesp:\1:$VERSION/g" README.md
+sed -i .bak -r "s/io\.github\.magonxesp:([a-z\-]+):v?[0-9.]+\.?[a-z]*\.?[0-9]*/io\.github\.magonxesp:\1:$VERSION/g" README.md
 git add README.md
 git commit -m "🚀 bump version to $VERSION"
 git tag "$VERSION"
