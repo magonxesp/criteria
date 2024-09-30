ee33ce57f8a617126ce1948cc1979255c0a75cdb
MagonxESP <janma.360@gmail.com>
2024-09-30 23:38:48 +0200

    test: disable mariadb tests

--- core/src/jvmTest/kotlin/io/github/magonxesp/criteria/infrastructure/exposed/MariaDBCriteriaExposedQueryAdapterTest.kt
+++ core/src/jvmTest/kotlin/io/github/magonxesp/criteria/infrastructure/exposed/MariaDBCriteriaExposedQueryAdapterTest.kt
@@ -2,11 +2,12 @@ package io.github.magonxesp.criteria.infrastructure.exposed
 
 import org.jetbrains.exposed.sql.Database
 
-class MariaDBCriteriaExposedQueryAdapterTest : CriteriaExposedQueryAdapterTest() {
-	override fun setupDatabaseConnection() = Database.connect(
-		mariadb.jdbcUrl,
-		driver = "org.mariadb.jdbc.Driver",
-		user = mariadb.username,
-		password = mariadb.password
-	)
-}
+// TODO: uncomment for test mariadb
+//class MariaDBCriteriaExposedQueryAdapterTest : CriteriaExposedQueryAdapterTest() {
+//	override fun setupDatabaseConnection() = Database.connect(
+//		mariadb.jdbcUrl,
+//		driver = "org.mariadb.jdbc.Driver",
+//		user = mariadb.username,
+//		password = mariadb.password
+//	)
+//}
